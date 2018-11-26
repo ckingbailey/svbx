7c395dab67e1fe268fc2cafba289dc140e5e68e7
ckb <ckingbailey@gmail.com>
2018-11-26 12:44:53 -0800

    use window.goback when new def closed and close info empty

--- js/window_hack.js
+++ js/window_hack.js
@@ -2,7 +2,7 @@ const WindowHack = function WindowHack() {
     return this
 }
 
-WindowHack.prototype.goBack = function goBack(message) {
+WindowHack.goBack = function goBack(message) {
     window.history.go(-1)
     window.alert(message)
 }
