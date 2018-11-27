c5f01faf88eabc88ab62d2bcb458d18e1e4970c9
ckb <ckingbailey@gmail.com>
2018-11-27 07:25:28 -0800

    reverse order of window.alert and history(-1)

--- src/WindowHack.php
+++ src/WindowHack.php
@@ -4,7 +4,7 @@ namespace SVBX;
 class WindowHack {
     static public function goBack(string $alertMsg) {
         echo "
-        <script src='/js/window_hack.js?v=1543264421231'></script>
+        <script src='/js/window_hack.js?v=1543276175810'></script>
         <script>WindowHack.goBack('$alertMsg')</script>";
     }
 }
