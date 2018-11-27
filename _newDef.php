ddcb6361a726b947f4d4ab6da91561def3171095
ckb <ckingbailey@gmail.com>
2018-11-27 07:24:45 -0800

    add twig | safe filter to new and update def

--- _newDef.php
+++ _newDef.php
@@ -31,6 +31,10 @@ $html_sanitize_decode = new Twig_Filter('html_sanitize_decode', function($str) {
 $filter_stripslashes = new Twig_Filter('unescape', function($str) {
     return stripcslashes($str);
 });    
+$filter_decode = new Twig_Filter('safe', function($str) {
+    return html_entity_decode($str);
+});
+$twig->addFilter($filter_decode);    
 $twig->addFilter($html_sanitize_decode);
 $twig->addFilter($filter_stripslashes);
     
