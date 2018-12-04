6fe6551132b9247c9c7ea8c5debf88641d81efd9
ckb <ckingbailey@gmail.com>
2018-12-04 14:45:04 -0800

    pass query data to dashboard template

--- dashboard.php
+++ dashboard.php
@@ -5,6 +5,7 @@ require 'WeeklyDelta.php';
 require 'session.php';
 
 $context = [
+  'session' => $_SESSION,
   'title' => 'Home',
   'pageHeading' => 'Database Information'
 ];
@@ -15,32 +16,32 @@ $context = [
 $link = new MysqliDb(DB_CREDENTIALS);
 
 $context['data']['status'] = $link->
-  orderBy('statusID')->
+  orderBy('statusID', 'ASC')->
   groupBy('statusName')->
   where('CDL.status', '3', '<>')->
   join('status s', 'CDL.status = s.statusID', 'LEFT')->
   get('CDL', null, ['statusName', 'COUNT(CDL.status) as count']);
 
 $context['data']['severity'] = $link->
-  orderBy('severityName')->
+  orderBy('severityName', 'ASC')->
   groupBy('severityName')->
   join('severity s', 'CDL.severity = s.severityID', 'LEFT')->
   get('CDL', null, ['severityName', 'COUNT(CDL.severity) as count']);
 
 $context['data']['system'] = $link->
-  orderBy('systemName')->
+  orderBy('systemName', 'ASC')->
   groupBy('systemName')->
   join('system s', 'CDL.groupToResolve = s.systemID', 'LEFT')->
   join('users_enc', 's.lead = users_enc.userid', 'LEFT')->
-  get('CDL', null, ['systemName', 'COUNT(CDL.groupToResolve) as count', 'CONCAT(SUBSTR(firstname, 1, 2), " ", lastname) as lead']);
+  get('CDL', null, ['systemName', 'COUNT(CDL.groupToResolve) as count', 'CONCAT(SUBSTR(firstname, 1, 1), " ", lastname) as lead']);
 
 $context['data']['location'] = $link->
-  orderBy('locationName')->
+  orderBy('locationName', 'ASC')->
   groupBy('locationName')->
   join('location l', 'CDL.location = l.locationID', 'LEFT')->
   get('CDL', null, ['locationName', 'COUNT(CDL.location) as count']);
 
-echo '<pre>' . print_r($context['data'], true) . '</pre>';
+// echo '<pre>' . print_r($context['data'], true) . '</pre>';
 
 // $sqlSys = "SELECT COUNT(*) FROM System"; //Systems Count
 // $sqlStat = "SELECT COUNT(*) FROM Status"; //Status Counts
@@ -56,10 +57,6 @@ echo '<pre>' . print_r($context['data'], true) . '</pre>';
 // $majSev = 0;
 // $minSev = 0;
 
-// array of tables to render as cards
-// each item MUST include TableName, PluralString, ItemString, SqlString
-// if any extra content is needed, add it to index after required items
-
 // foreach($cards as $card) {
 //   $tableStr = 'SELECT COUNT(*) FROM ' . $card[0];
 //   $res = $link->query($tableStr);
@@ -70,16 +67,15 @@ echo '<pre>' . print_r($context['data'], true) . '</pre>';
 //   $res->close();
 // }
 
-// // instantiate Twig
-// $loader = new Twig_Loader_Filesystem('templates');
-// $twig = new Twig_Environment($loader, [
-//     'debug' => getenv('PHP_ENV') === 'dev'
-// ]);
-// if (getenv('PHP_ENV') === 'dev') $twig->addExtension(new Twig_Extension_Debug());
+// instantiate Twig
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, [
+    'debug' => getenv('PHP_ENV') === 'dev'
+]);
+if (getenv('PHP_ENV') === 'dev') $twig->addExtension(new Twig_Extension_Debug());
 
-// // instantiate report object
-// $weeklySIT3delta = new WeeklyDelta('SIT3');
+// instantiate report object
+$weeklySIT3delta = new WeeklyDelta('SIT3');
+$context['data']['weeklyReport'] = $weeklySIT3delta->getData();
 
-// $twig->display('dashboard.html.twig', [
-//   'data' => $weeklySIT3delta->getData()
-// ]);
+$twig->display('dashboard.html.twig', $context);
