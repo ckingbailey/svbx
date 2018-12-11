bd16addfbc83d5f55198a67625cc400b7f582f8b
ckb <ckingbailey@gmail.com>
2018-12-10 16:10:09 -0800

    check for format in qs

--- api/report.php
+++ api/report.php
@@ -2,13 +2,29 @@
 use SVBX\Report;
 use SVBX\Export;
 
-require '../vendor/autoload.php';
+$basedir = __DIR__ . '/..';
+require "$basedir/vendor/autoload.php";
+require "$basedir/session.php";
 
-// TODO: authorize requester using session cookie
+/* query params:
+**  milestone
+**  date
+**  system
+**  type
+**  format => if Export class lacks $format method, return bad http response
+*/
 // TODO: clean data
 // TODO: validate query params
 // TODO: check `format` param
-echo Export::csv(
+if (!method_exists('SVBX\Export', $_GET['format'])) {
+    header('Malformed request', true, 400);
+    exit;
+}
+
+$format = $_GET['format'];
+$reportType = 'delta';
+
+echo Export::$format(
     Report::delta($_GET['milestone'])->get()
 );
 
