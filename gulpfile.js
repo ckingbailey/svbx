3a35b08d2f00e7ee2735bb3e148bfa54ec5043a9
ckb <ckingbailey@gmail.com>
2018-12-14 15:10:50 -0800

    condition in gulpfile to handle prod

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,18 @@ const { dependencies } = require('./package.json')
 // TODO: gulp-merge may be the plugin needed for sending multiple srcs to multiples dests
 
 const css = () => {
+    const destPath = (process.env.NODE_ENV === 'prod' ? 'public_html/' : '')
+        + 'assets/css/'
+
     const tasks = Object.keys(dependencies).reduce((acc, dep) => {
         // TODO: check for presence of css folder in dist folder
         // TODO: check for existence of *.min.css for each file in dist/css
         // TODO: pipe the files to dest('assets/css/')
-        const path = fs.existsSync(`node_modules/${dep}/dist`) && `node_modules/${dep}/dist`
-        if (fs.existsSync(`${path}/css`)){
-            console.log('   path = ' + path + '/css')
-            acc.push(src(`${path}/css/*.css`)
-                .pipe(dest('assets/css/')))
+        const srcPath = fs.existsSync(`node_modules/${dep}/dist`) && `node_modules/${dep}/dist`
+        if (fs.existsSync(`${srcPath}/css`)){
+            console.log('   srcPath = ' + srcPath + '/css')
+            acc.push(src(`${srcPath}/css/*.css`)
+                .pipe(dest(destPath)))
         }
         return acc
     }, [])
@@ -23,12 +26,15 @@ const css = () => {
 }
 
 const js = () => {
+    const destPath = (process.env.NODE_ENV === 'prod' ? 'public_html/' : '')
+        + 'assets/js/'
+
     const tasks = Object.keys(dependencies).reduce((acc, dep) => {
-        const path = fs.existsSync(`node_modules/${dep}/dist`) && `node_modules/${dep}/dist`
-        if (fs.existsSync(`${path}/js`)) {
-            console.log('   path = ' + path + '/js')
-            acc.push(src(`${path}/js/*.js`)
-                .pipe(dest('./assets/js/')))
+        const srcPath = fs.existsSync(`node_modules/${dep}/dist`) && `node_modules/${dep}/dist`
+        if (fs.existsSync(`${srcPath}/js`)) {
+            console.log('   srcPath = ' + srcPath + '/js')
+            acc.push(src(`${srcPath}/js/*.js`)
+                .pipe(dest(destPath)))
         }
         return acc
     }, [])
