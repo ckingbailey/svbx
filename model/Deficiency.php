df14ee727a72a784836905cf37cbe7d35a75a1ab
ckb <ckingbailey@gmail.com>
2019-01-10 10:10:27 -0800

    only validate doc link if not empty

--- model/Deficiency.php
+++ model/Deficiency.php
@@ -358,7 +358,8 @@ class Deficiency
         $this->validateModInfo($action);
         $this->validateRequiredInfo($action);
         $this->validateClosureInfo();
-        if (filter_var($this->props['evidenceLink'], FILTER_VALIDATE_URL) === false)
+        if (!empty($this->props['evidenceLink'])
+            && filter_var($this->props['evidenceLink'], FILTER_VALIDATE_URL) === false)
             throw new \Exception('Evidence link must be valid URL');
     }
 
