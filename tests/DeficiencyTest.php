8acafa0f3e3c761d33d5898be917c96066aecd9c
ckb <ckingbailey@gmail.com>
2019-08-22 17:28:06 -0700

    rm failed attempt to manually delete test-inserted recs

--- tests/DeficiencyTest.php
+++ tests/DeficiencyTest.php
@@ -1,27 +1,11 @@
 <?php
 declare(strict_types=1);
-error_reporting(E_ALL & ~E_NOTICE);
 
 use SVBX\Deficiency;
 use PHPUnit\Framework\TestCase;
 
 final class DeficiencyTest extends TestCase
 {
-    private $newDefIDs = [];
-    private $db;
-
-    protected function setUp(): void
-    {
-        $this->db = new MysqliDb(DB_CREDENTIALS);
-        $this->db->startTransaction();
-    }
-
-    protected function tearDown(): void
-    {
-        $this->db->rollback();
-        $this->db->disconnect();
-    }
-
     public function testCanCreateNewWithRequiredProps(): void
     {
         $this->assertInstanceOf(
@@ -68,25 +52,23 @@ final class DeficiencyTest extends TestCase
 
     public function testCanInsertNewWithStatusOpen(): void
     {
-        array_push(
-            $this->newDefIDs,
-            $newDefID = (new Deficiency(false, [
-                'safetyCert' => 1,
-                'systemAffected' => 1,
-                'location' => 1,
-                'specLoc' => 'test_specLoc',
-                'status' => 1,
-                'severity' => 1,
-                'dueDate' => date('Y-m-d'),
-                'groupToResolve' => 1,
-                'requiredBy' => 1,
-                'contractID' => 1,
-                'identifiedBy' => 'ckb',
-                'defType' => 1,
-                'description' => 'test_description',
-                'created_by' => 'demo', // required creation info
-            ]))->insert()
-        );
+        $newDefID = (new Deficiency(false, [
+            'safetyCert' => 1,
+            'systemAffected' => 1,
+            'location' => 1,
+            'specLoc' => 'test_specLoc',
+            'status' => 1,
+            'severity' => 1,
+            'dueDate' => date('Y-m-d'),
+            'groupToResolve' => 1,
+            'requiredBy' => 1,
+            'contractID' => 1,
+            'identifiedBy' => 'ckb',
+            'defType' => 1,
+            'description' => 'test_description',
+            'created_by' => 'demo', // required creation info
+        ]))->insert();
+        
         $this->assertNotEquals(intval($newDefID), 0);
 
         $newDef = new Deficiency($newDefID);
@@ -104,28 +86,25 @@ final class DeficiencyTest extends TestCase
 
     public function testInsertWithStatusClosedGetsTimestamp(): void
     {
-        array_push(
-            $this->newDefIDs,
-            $newDefID = (new Deficiency(false, [
-                'safetyCert' => 1,
-                'systemAffected' => 1,
-                'location' => 1,
-                'specLoc' => 'test_specLoc',
-                'status' => 4,
-                'severity' => 1,
-                'dueDate' => date('Y-m-d'),
-                'groupToResolve' => 1,
-                'requiredBy' => 1,
-                'contractID' => 1,
-                'identifiedBy' => 'ckb',
-                'defType' => 1,
-                'description' => 'test_description',
-                'created_by' => 'demo', // required creation info
-                'repo' => 1, // required closure info, fk
-                'evidenceType' => 1, // required closure info, fk
-                'evidenceID' => 'aaa000-bbb999' // required closure info
-            ]))->insert()
-        );
+        $newDefID = (new Deficiency(false, [
+            'safetyCert' => 1,
+            'systemAffected' => 1,
+            'location' => 1,
+            'specLoc' => 'test_specLoc',
+            'status' => 4,
+            'severity' => 1,
+            'dueDate' => date('Y-m-d'),
+            'groupToResolve' => 1,
+            'requiredBy' => 1,
+            'contractID' => 1,
+            'identifiedBy' => 'ckb',
+            'defType' => 1,
+            'description' => 'test_description',
+            'created_by' => 'demo', // required creation info
+            'repo' => 1, // required closure info, fk
+            'evidenceType' => 1, // required closure info, fk
+            'evidenceID' => 'aaa000-bbb999' // required closure info
+        ]))->insert();
 
         $this->assertNotEquals(intval($newDefID), 0);
         
